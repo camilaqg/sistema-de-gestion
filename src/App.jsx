aa6d05a8f74923083679b3ce56e7d90fc72becf8
Camila Quinones <camiqg2905@gmail.com>
2026-05-05 17:56:42 -0500

    CREACION DE PROYECTO

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './pages/Login';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Login/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
