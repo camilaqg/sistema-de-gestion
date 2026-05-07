cba657cf87605032cd492c98f772d3da086d7d8e
Camila Quinones <camiqg2905@gmail.com>
2026-05-07 13:45:07 -0500

    refactor: remove router.tsx and  change all to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router.tsx'
+import { App } from './App'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={App} />
   </StrictMode>,
 )
